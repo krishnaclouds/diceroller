d71bcae4582c8e8ccf6424666cb22d46ae345b42
Bala Krishna <balakrishna@docsapp.in>
2021-11-01 07:08:35 +0530

    Added androidx resource tool to have png compatability below sdk 21

--- app/src/main/java/in/medibuddy/diceroller/MainActivity.kt
+++ app/src/main/java/in/medibuddy/diceroller/MainActivity.kt
@@ -4,20 +4,18 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageView
-import android.widget.TextView
-import android.widget.Toast
 import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var diceImage : ImageView
+    private lateinit var diceImage : ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val rollButton : Button = findViewById(R.id.roll_button)
-        rollButton.text = "Roll Dice"
+        rollButton.text = getString(R.string.rollDiceString)
 
         diceImage = findViewById(R.id.dice_image)
 
@@ -27,7 +25,6 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun rollDice() {
-//        Toast.makeText(this, "Dice Rolled", 1).show()
         val drawableImage = when (Random.nextInt(6) + 1) {
             1 -> R.drawable.dice_1
             2 -> R.drawable.dice_2
@@ -37,7 +34,6 @@ class MainActivity : AppCompatActivity() {
             6 -> R.drawable.dice_6
             else -> R.drawable.empty_dice
         }
-//        val diceImage : ImageView = findViewById(R.id.dice_image)
         diceImage.setImageResource(drawableImage)
     }
 }
